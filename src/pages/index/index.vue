e50459a2c7f846d03da0ae8e20eff1fa8be0cf1a
Foluolaaa <348064730@qq.com>
2024-06-23 20:02:38 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="u-wrap">
+		<swiper class="swiper-container" circular :indicator-dots="indicatorDots" :autoplay="autoplay"
+			:interval="interval" :duration="duration">
+			<swiper-item v-for="(item, index) in swipperList">
+				<image class="imgs" :src="item.image"></image>
+			</swiper-item>
+
+		</swiper>
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from 'vue';
+	const indicatorDots = ref(true)
+	const autoplays = ref(true)
+	const intervals = ref(2000)
+	const durations = ref(500)
+	// 轮播图数据
+	const swipperList = ref([{
+		image: '/static/image/swiper1.png'
+	}, {
+		image: '/static/image/swiper2.jpg'
+	}, {
+		image: '/static/image/swiper3.jpg'
+	}])
+</script>
+
+<style lang='scss'>
+	.swiper-container {
+		height: 180px;
+
+		.item {
+			height: 180px;
+		}
+	}
+</style>
